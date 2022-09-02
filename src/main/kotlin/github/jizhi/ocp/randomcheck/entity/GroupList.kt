4168d0a5e206579d35235c9be853185ae78bd7ae
JiZhi <2590970043@qq.com>
2022-09-02 13:28:54 +0800

    update GroupList entity class

--- src/main/kotlin/github/jizhi/ocp/randomcheck/entity/GroupList.kt
+++ src/main/kotlin/github/jizhi/ocp/randomcheck/entity/GroupList.kt
@@ -1,29 +1,31 @@
 package github.jizhi.ocp.randomcheck.entity
 
+import com.alibaba.fastjson2.annotation.JSONField
+
 /**
  * @author  JiZhi
  * @date  2022/9/2 11:49
  * @version  V1.0
  **/
 data class GroupList(
-//    @JSONField(name = "group_create_time")
+    @JSONField(name = "group_create_time")
     private var groupCreateTime: String? = null,
 
-//    @JSONField(name = "group_id")
+    @JSONField(name = "group_id")
     private val groupId: String? = null,
 //
-//    @JSONField(name = "group_level")
+    @JSONField(name = "group_level")
     private val groupLevel: String? = null,
 
-//    @JSONField(name = "group_memo")
+    @JSONField(name = "group_memo")
     private val groupMemo: String? = null,
 
-//    @JSONField(name = "group_name")
+    @JSONField(name = "group_name")
     private val groupName: String? = null,
 
-//    @JSONField(name = "max_member_count")
+    @JSONField(name = "max_member_count")
     private val maxMemberCount: String? = null,
 
-//    @JSONField(name = "member_count")
+    @JSONField(name = "member_count")
     private val memberCount: String? = null
 )
